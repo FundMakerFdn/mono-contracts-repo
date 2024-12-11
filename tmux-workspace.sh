9fb8bc19a56507dc9eeda67de118e385671fc554
MAX-OTC <maxgctr@gmail.com>
2024-12-11 15:27:24 +0100

    tmux update

--- tmux-workspace.sh
+++ tmux-workspace.sh
@@ -21,6 +21,7 @@ tmux split-window -v -t $SESSION_NAME:0.0
 tmux split-window -v -t $SESSION_NAME:0.3
 
 
+
 tmux resize-pane -t $SESSION_NAME:0.0 -x 10
 tmux resize-pane -t $SESSION_NAME:0.2 -x 25
 
@@ -36,7 +37,8 @@ tmux send-keys -t $SESSION_NAME:0.1 "sleep 5; yarn miner" C-m
 tmux send-keys -t $SESSION_NAME:0.2 "sleep 7; yarn deploy" C-m
 
 # Right pane (top)
-tmux send-keys -t $SESSION_NAME:0.3 "sleep 10; yarn addSymm 0; yarn addSymm 1" C-m
+tmux send-keys -t $SESSION_NAME:0.3 "sleep 10; yarn addSymm 0; sleep 2; yarn pSymmA" C-m
+tmux send-keys -t $SESSION_NAME:0.4 "sleep 10; yarn addSymm 1; sleep 2; yarn pSymmB" C-m
 
 # Attach to the session
 tmux attach -t $SESSION_NAME
