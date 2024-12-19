4a82d80c510668da0bc416a174eedfcd8ac91b14
Vladyslav Havriuk <web.user@ukr.net>
2024-12-19 04:20:15 +0100

    Run in network localhost (tmux bugfix)

--- tmux/party.sh
+++ tmux/party.sh
@@ -3,4 +3,4 @@
 SCENARIO="${1:-1.0}" # Scenario 1.0 by default
 PARTY="${2:-A}" # Party A by default
 
-yarn hardhat run "pSymm/demo/scenario$SCENARIO/party$PARTY.js"
+yarn hardhat run "pSymm/demo/scenario$SCENARIO/party$PARTY.js" --network localhost
