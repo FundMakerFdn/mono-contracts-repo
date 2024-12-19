300908557102bc85ff78fce7228a21e7e95ae24c
Vladyslav Havriuk <web.user@ukr.net>
2024-12-19 04:16:24 +0100

    Yarn party shortcut

---
+++ tmux/party.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCENARIO="${1:-1.0}" # Scenario 1.0 by default
+PARTY="${2:-A}" # Party A by default
+
+yarn hardhat run "pSymm/demo/scenario$SCENARIO/party$PARTY.js"
--- tmux/scenario.sh
+++ tmux/scenario.sh
@@ -5,7 +5,7 @@
 SCENARIO="${1:-1.0}" # Scenario 1.0 by default
 
 # Right pane (top)
-tmux send-keys -t $SESSION_NAME:0.3 "sleep 10; yarn addSymm 0; sleep 3; yarn hardhat run pSymm/demo/scenario$SCENARIO/partyA.js" C-m
-tmux send-keys -t $SESSION_NAME:0.4 "sleep 10; yarn addSymm 1; yarn hardhat run pSymm/demo/scenario$SCENARIO/partyB.js" C-m
+tmux send-keys -t $SESSION_NAME:0.3 "sleep 10; yarn addSymm 0; sleep 3; yarn party $SCENARIO A" C-m
+tmux send-keys -t $SESSION_NAME:0.4 "sleep 10; yarn addSymm 1; yarn party $SCENARIO B" C-m
 
 tmux attach -t $SESSION_NAME
