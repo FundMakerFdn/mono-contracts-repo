9e162bd6bd9aec05566a532eb5dec602502f1eb9
Max OTC <58943684+Max-OTC@users.noreply.github.com>
2025-01-12 02:04:42 +0100

    Update SolverRegistry.sol

--- apps/contracts/src/SettleMaker/SolverRegistry.sol
+++ apps/contracts/src/SettleMaker/SolverRegistry.sol
@@ -7,12 +7,14 @@ contract SolverRegistry {
         string ipAddress;
         uint8 solverType;
     }
-    
+
     event SolverRegistered(address indexed solver, string ipAddress);
     event SolverRemoved(address indexed solver);
+    event ReputationSet(address indexed solver, address indexed kycProvider, uint256 score);
 
     mapping(address => SolverData) public solvers;
-    mapping(address => mapping( address => uint8)) kycTypes;
+    mapping(address => mapping( address => uint256)) public reputation;
+    mapping(address => mapping( address => uint8)) public kycTypes;
     
     /// @notice Register as a solver with IP address
     /// @param solverData The solver's data
@@ -28,6 +30,14 @@ contract SolverRegistry {
         kycTypes[msg.sender][kycProvider] = kycType;
     }
 
+    /// @notice Set reputation for a solver ( See it as an Amazon review rating)
+    /// @param kycProvider The KYC provider's address
+    /// @param score The reputation score
+    function setReputation(address kycProvider, uint256 score) external virtual{
+        reputation[msg.sender][kycProvider] = score;
+        emit ReputationSet(msg.sender, kycProvider, score);
+    }
+
     /// @notice Get solver's IP address
     function getSolver(address solver) external virtual view returns (string memory ipAddress, uint8 solverType){
         return (solvers[solver].ipAddress, solvers[solver].solverType);
@@ -36,5 +46,9 @@ contract SolverRegistry {
     /// @notice Get KYC type for a solver
     function getKycType(address solver, address kycProvider) external virtual view returns (uint8 kycType){
         return kycTypes[solver][kycProvider];
+    }
+
+    function getReputation(address solver, address kycProvider) external virtual view returns (uint256 score){
+        return reputation[solver][kycProvider];
     }
 }
