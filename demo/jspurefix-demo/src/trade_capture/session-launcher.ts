440ab8a2e751fc733e35a7dbeb449f2db1170d07
Vladyslav Havriuk <web.user@ukr.net>
2025-02-17 00:25:00 +0100

    Remove old purefix code

--- demo/jspurefix-demo/src/trade_capture/session-launcher.ts
+++
@@ -1,7 +0,0 @@
-import { SessionLauncher as BaseLauncher, IJsFixConfig } from "jspurefix";
-import { IExtendedFixConfig } from "./types";
-import { EngineFactory } from "jspurefix";
-
-export abstract class SessionLauncher extends BaseLauncher {
-  protected abstract makeFactory(config: IJsFixConfig): EngineFactory;
-}
--- demo/jspurefix-demo/src/trade_capture/types.ts
+++
@@ -1,7 +0,0 @@
-import { IJsFixConfig, ISessionDescription } from "jspurefix";
-
-export interface IExtendedFixConfig extends IJsFixConfig {
-  description: ISessionDescription & {
-    walletId?: number;
-  };
-}
