d86d4b39429d55a9fee13432c724fb2870424870
Vladyslav Havriuk <web.user@ukr.net>
2025-02-18 22:56:47 +0100

    PPM builder

--- apps/PPM/PPMs/ppmBuilder.js
+++ apps/PPM/PPMs/ppmBuilder.js
@@ -1,43 +1,44 @@
-// --- Utility to Expand Combinations ---
-// Looks for "party", "chainId", and "state" keys.
-// If any of these are arrays, produce one object per combination.
-function expandObject(obj) {
-    const keysToExpand = ["party", "chainId", "state"];
-    const arrays = {};
-    keysToExpand.forEach((key) => {
-      if (obj.hasOwnProperty(key)) {
-        arrays[key] = Array.isArray(obj[key]) ? obj[key] : [obj[key]];
-      }
-    });
-    const expandKeys = Object.keys(arrays);
-    if (expandKeys.length === 0) return [obj];
-  
-    const combinations = [];
-    function helper(index, current) {
-      if (index === expandKeys.length) {
-        combinations.push({ ...current });
-        return;
-      }
-      const key = expandKeys[index];
-      arrays[key].forEach((value) => {
-        current[key] = value;
-        helper(index + 1, current);
-      });
-    }
-    helper(0, {});
-  
-    // Merge each combination with non-expanded properties.
-    return combinations.map((combo) => {
-      const clone = { ...obj };
-      expandKeys.forEach((key) => delete clone[key]);
-      return { ...clone, ...combo };
-    });
+class PPMBuilder {
+  constructor() {
+    this.ppmItems = [];
   }
 
-  function addPPM(item) {
-    const expanded = expandObject(item);
-    ppmItems.push(...expanded);
+  // Expands object combinations for array values in party, chainId, and state
+  expandObject(obj) {
+    const keysToExpand = ["party", "chainId", "state"];
+
+    const arrays = Object.fromEntries(
+      keysToExpand
+        .filter((key) => key in obj)
+        .map((key) => [key, Array.isArray(obj[key]) ? obj[key] : [obj[key]]])
+    );
+
+    if (Object.keys(arrays).length === 0) return [obj];
+
+    const combinations = Object.entries(arrays).reduce(
+      (acc, [key, values]) => {
+        return acc.flatMap((combo) =>
+          values.map((value) => ({ ...combo, [key]: value }))
+        );
+      },
+      [{}]
+    );
+
+    const baseObj = { ...obj };
+    keysToExpand.forEach((key) => delete baseObj[key]);
+
+    return combinations.map((combo) => ({ ...baseObj, ...combo }));
+  }
+
+  addPPM(item) {
+    const expanded = this.expandObject(item);
+    this.ppmItems.push(...expanded);
     return expanded;
   }
-  
-  export { addPPM };
+
+  getPPM() {
+    return this.ppmItems;
+  }
+}
+
+export { PPMBuilder };
