0ae2add10240bfedbf1fe4d65b66c937f4d12042
Vladyslav Havriuk <web.user@ukr.net>
2025-02-20 06:28:48 +0100

    MockPPM deploy test, cleanup package.json

---
+++ contracts/test/MockPPM/MockPPM.deployment.js
@@ -0,0 +1,31 @@
+const assert = require("node:assert/strict");
+const { loadFixture } = require("@nomicfoundation/hardhat-toolbox-viem/network-helpers");
+const hre = require("hardhat");
+
+async function deployFixture() {
+  const [deployer, user1, user2] = await hre.viem.getWalletClients();
+  const publicClient = await hre.viem.getPublicClient();
+
+  // Deploy MockPPM
+  const mockPPM = await hre.viem.deployContract("MockPPM");
+
+  return {
+    mockPPM,
+    deployer,
+    user1,
+    user2,
+    publicClient
+  };
+}
+
+function shouldDeployMockPPM() {
+  it("should deploy successfully", async function () {
+    const { mockPPM } = await loadFixture(deployFixture);
+    assert.ok(mockPPM.address, "MockPPM not deployed");
+  });
+}
+
+module.exports = {
+  shouldDeployMockPPM,
+  deployFixture
+};
---
+++ contracts/test/MockPPM/MockPPM.test.js
@@ -0,0 +1,7 @@
+const { shouldDeployMockPPM } = require("./MockPPM.deployment");
+
+describe("MockPPM", function () {
+  describe("Mock PPM Deployment", function () {
+    shouldDeployMockPPM();
+  });
+});
