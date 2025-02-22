fa8cb0d7aeaac0db871ede55454f423f2246bb37
Vladyslav Havriuk <web.user@ukr.net>
2025-02-22 03:28:18 +0100

    Noir test

---
+++ contracts/test/NoirTest/noirtest.js
@@ -0,0 +1,73 @@
+const assert = require("node:assert/strict");
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox-viem/network-helpers");
+const { bytesToHex } = require("viem");
+const { UltraPlonkBackend } = require("@aztec/bb.js");
+const { Noir } = require("@noir-lang/noir_js");
+const path = require("path");
+const fs = require("fs");
+const hre = require("hardhat");
+
+let iota = 0;
+
+async function deployFixture() {
+  const [deployer] = await hre.viem.getWalletClients();
+  const publicClient = await hre.viem.getPublicClient();
+
+  // Initialize the Barretenberg backend
+  const jsondata = JSON.parse(
+    fs.readFileSync(
+      path.resolve(__dirname, "../../../noir/pSymm/target/pSymm.json")
+    )
+  );
+  const backend = new UltraPlonkBackend(jsondata.bytecode);
+  const noir = new Noir(jsondata);
+
+  // Deploy NoirTest contract
+  const noirTest = await hre.viem.deployContract("NoirTest");
+
+  return {
+    noirTest,
+    noir,
+    backend,
+    deployer,
+    publicClient,
+  };
+}
+
+describe("NoirTest", function () {
+  it("Should verify a valid proof", async function () {
+    const { noirTest, backend, noir } = await loadFixture(deployFixture);
+
+    const x = 5;
+    const y = 6; // x != y
+
+    // Generate the proof
+
+    console.log("generating witness...");
+    const { witness } = await noir.execute({ x, y });
+    console.log("generating proof...");
+    const { proof, publicInputs } = await backend.generateProof(witness);
+
+    const proofHex = bytesToHex(proof);
+    const result = await noirTest.read.verify([proofHex, publicInputs[0]]);
+    assert.equal(result, true, "Valid proof should verify");
+  });
+
+  // it("Should reject an invalid proof", async function () {
+  //   const { noirTest, circuit } = await loadFixture(deployFixture);
+
+  //   const x = 5n;
+  //   const wrongY = 26n; // incorrect result
+
+  //   // Generate the proof with wrong public input
+  //   const proof = await circuit.generateFinalProof({ x }, wrongY);
+
+  //   // const result = await noirTest.read.verify([proof, wrongY]);
+  //   // assert.equal(result, false, "Invalid proof should not verify");
+  // });
+});
+//
+
+// module.exports = { deployFixture };
