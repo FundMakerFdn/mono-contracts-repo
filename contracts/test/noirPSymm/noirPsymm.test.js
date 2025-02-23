ecf62e1ba03da52c98018533201706ac7d1ad9c6
MAX-OTC <maxgctr@gmail.com>
2025-02-22 22:55:48 -0500

    noir Psymm, ecdsa

---
+++ contracts/test/noirPSymm/noirPsymm.test.js
@@ -0,0 +1,7 @@
+const { shouldDeployNoirPsymm } = require("./noirPsymm.deployment");
+
+describe("noirPsymm", function () {
+  describe("noirPsymm Deployment", function () {
+    shouldDeployNoirPsymm();
+  });
+});
