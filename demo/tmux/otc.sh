d7fb32fc06dff29ae5a964b5c1a331c748beb563
Vladyslav Havriuk <web.user@ukr.net>
2025-04-01 09:24:38 +0200

    tmux

--- demo/tmux/otc.sh
+++ demo/tmux/otc.sh
@@ -14,6 +14,7 @@ tmux split-window -h -t $SESSION_NAME:0
 # Split panels horizontally where needed
 tmux split-window -v -t $SESSION_NAME:0.0
 tmux split-window -v -t $SESSION_NAME:0.2
+tmux split-window -v -t $SESSION_NAME:0.4
 
 # Resize panels to make them more evenly distributed
 tmux resize-pane -t $SESSION_NAME:0.0 -x "33%"
@@ -24,7 +25,7 @@ tmux resize-pane -t $SESSION_NAME:0.2 -x "33%"
 tmux send-keys -t $SESSION_NAME:0.0 "yarn hardhat node" C-m
 
 # Second panel (bottom-left) - Deploy pSymm
-tmux send-keys -t $SESSION_NAME:0.1 "sleep 5; yarn deploy-psymm; node apps/PSYMM-OTC/guardian.js 127.0.0.4 3" C-m
+tmux send-keys -t $SESSION_NAME:0.1 "sleep 5; yarn deploy-psymm" C-m
 
 # Third panel (top-middle) - Start solver
 tmux send-keys -t $SESSION_NAME:0.2 "sleep 8; yarn solver" C-m
@@ -32,8 +33,11 @@ tmux send-keys -t $SESSION_NAME:0.2 "sleep 8; yarn solver" C-m
 # Fourth panel (bottom-middle) - Guardian
 tmux send-keys -t $SESSION_NAME:0.3 "sleep 7; node apps/PSYMM-OTC/guardian.js 127.0.0.3 2" C-m
 
-# Fifth panel (right) - Start trader
+# Fifth panel (top-right) - Start trader
 tmux send-keys -t $SESSION_NAME:0.4 "sleep 10; yarn trader" C-m
+
+# Sixth panel (bottom-right) - Guardian
+tmux send-keys -t $SESSION_NAME:0.5 "sleep 7; node apps/PSYMM-OTC/guardian.js 127.0.0.4 3" C-m
 
 # Attach to the session
 tmux attach -t $SESSION_NAME
