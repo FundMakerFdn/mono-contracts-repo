01df864e4aee1e20920b31050f7b724e0fe2de1b
Vladyslav Havriuk <web.user@ukr.net>
2025-04-05 19:36:09 +0200

    seqnumcheck

--- libs/pSymmFIX/session-manager.js
+++ libs/pSymmFIX/session-manager.js
@@ -1,48 +1,77 @@
 class Session {
-    constructor(checks = []) {
-        this.checks = checks;
-        this.messages = [];
-        this.lastSeqNums = new Map(); // Track last sequence number per sender
+  constructor(checks = []) {
+    this.checks = checks;
+    this.messages = [];
+  }
+
+  handleMessage(message) {
+    // Run all validation checks
+    for (const check of this.checks) {
+      const result = check.verify(message);
+      if (!result) {
+        throw new Error(`Message failed validation check: ${check.name}`);
+      }
     }
 
-    handleMessage(message) {
-        // Run all validation checks
-        for (const check of this.checks) {
-            const result = check(message);
-            if (!result) {
-                throw new Error(`Message failed validation check: ${check.name}`);
-            }
-        }
+    if (message.MsgType === "ACK") {
+      // Find and update the acknowledged message
+      const targetMessage = this.messages.find(
+        (msg) =>
+          msg.SenderCompID === message.TargetCompID &&
+          msg.MsgSeqNum === message.RefMsgSeqNum
+      );
 
-        const senderCompId = message.SenderCompID;
-        
-        if (message.MsgType === 'ACK') {
-            // Find and update the acknowledged message
-            const targetMessage = this.messages.find(msg => 
-                msg.SenderCompID === message.TargetCompID && 
-                msg.MsgSeqNum === message.RefMsgSeqNum
-            );
-            
-            if (targetMessage) {
-                targetMessage.signature = message.signature;
-            }
-        } else {
-            // Add new message
-            this.messages.push(message);
-        }
-
-        // Update sequence number for this sender
-        const seqNum = parseInt(message.MsgSeqNum, 10);
-        this.lastSeqNums.set(senderCompId, seqNum);
+      if (targetMessage) {
+        targetMessage.signature = message.signature;
+      }
+    } else {
+      // Add new message
+      this.messages.push(message);
     }
+  }
 
-    getLastSeqNum(senderCompId) {
-        return this.lastSeqNums.get(senderCompId) || 0;
-    }
-
-    getMessages() {
-        return [...this.messages];
-    }
+  getMessages() {
+    return [...this.messages];
+  }
 }
 
-module.exports = Session;
+class SeqNumCheck {
+  constructor() {
+    this.name = "SeqNumCheck";
+    this.seqNums = new Map();
+  }
+
+  verify(message) {
+    const sender = message.SenderCompID;
+    const seqNum = message.MsgSeqNum;
+
+    if (message.MsgType === "ACK") {
+      // For ACK messages, verify that RefMsgSeqNum matches the last sequence number
+      // from the target party
+      const targetLastSeq = this.seqNums.get(message.TargetCompID);
+      if (targetLastSeq !== message.RefMsgSeqNum) {
+        return false;
+      }
+    }
+
+    // Get the last sequence number for this sender
+    const lastSeqNum = this.seqNums.get(sender) || 0;
+
+    // Verify sequence number is one more than the last one
+    if (seqNum !== lastSeqNum + 1) {
+      return false;
+    }
+
+    // Update the sequence number for this sender
+    this.seqNums.set(sender, seqNum);
+    return true;
+  }
+}
+
+function createSession() {
+  const seqNumCheck = new SeqNumCheck();
+  const session = new Session([seqNumCheck]);
+  return session;
+}
+
+module.exports = { createSession, Session, SeqNumCheck };
